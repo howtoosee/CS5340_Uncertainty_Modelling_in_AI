a604e394a48c8f3db19619683fe2f7fa5ef41fd2
Chen Xihao <xihao@weisshorn.comp.nus.edu.sg>
2024-04-25 22:18:19 +0800

    Update baseline

--- baseline/data_module.py
+++ baseline/data_module.py
@@ -186,9 +186,9 @@ def get_cifar10_train():
     }
 
 
-def get_cifar10_near(is_train=False):
+def get_cifar10_near(is_train=False, proportion=PROPORTION):
     id_data = Cifar10(is_train=is_train, label_offset=0, ood_label=0)
-    n = int(len(id_data) * PROPORTION)
+    n = int(len(id_data) * proportion)
     ood_data = Cifar100(is_train=is_train, label_offset=10, ood_label=1).sample(n)
     return {
         "dataset": ConcatDataset([id_data, ood_data]),
@@ -197,9 +197,9 @@ def get_cifar10_near(is_train=False):
     }
 
 
-def get_cifar10_far(is_train=False):
+def get_cifar10_far(is_train=False, proportion=PROPORTION):
     id_data = Cifar10(is_train=is_train, label_offset=0, ood_label=0)
-    n = int(len(id_data) * PROPORTION / 3)
+    n = int(len(id_data) * proportion / 3)
 
     ood_data = ConcatDataset(
         [
@@ -223,9 +223,9 @@ def get_mnist_train():
     }
 
 
-def get_mnist_near(is_train=False):
+def get_mnist_near(is_train=False, proportion=PROPORTION):
     id_data = MnistDataset(is_train=is_train, label_offset=0, ood_label=0)
-    n = int(len(id_data) * PROPORTION / 2)
+    n = int(len(id_data) * proportion / 2)
     ood_data = ConcatDataset(
         [
             FashionMnistDataset(is_train=is_train, label_offset=10, ood_label=1).sample(n),
@@ -240,9 +240,9 @@ def get_mnist_near(is_train=False):
     }
 
 
-def get_mnist_far1(is_train=False):
+def get_mnist_far1(is_train=False, proportion=PROPORTION):
     id_data = MnistDataset(is_train=is_train, label_offset=0, ood_label=0)
-    n = int(len(id_data) * PROPORTION / 2)
+    n = int(len(id_data) * proportion / 2)
     ood_data = ConcatDataset(
         [
             Cifar10(is_train=is_train, label_offset=10, ood_label=1).sample(n),
@@ -256,9 +256,9 @@ def get_mnist_far1(is_train=False):
     }
 
 
-def get_mnist_far2(is_train=False):
+def get_mnist_far2(is_train=False, proportion=PROPORTION):
     id_data = MnistDataset(is_train=is_train, label_offset=0, ood_label=0)
-    n = int(len(id_data) * PROPORTION)
+    n = int(len(id_data) * proportion)
     ood_data = Cifar10(is_train=is_train, label_offset=10, ood_label=1).sample(n)
     return {
         "dataset": ConcatDataset([id_data, ood_data]),
